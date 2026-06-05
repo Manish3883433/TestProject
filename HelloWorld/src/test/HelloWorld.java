20b52d6bb247f8b9e94865bcfd5dd23fe3608692
Manish Manish <manish154154@gmail.com>
2026-06-05 17:48:47 +0530

    Session-2 SDET QA video

--- HelloWorld/src/test/HelloWorld.java
+++ HelloWorld/src/test/HelloWorld.java
@@ -12,6 +12,37 @@ public class HelloWorld {
 		System.out.println("Welcome to java");
 		System.out.println("Welcome to java");
 		System.out.println("Welcome to java");*/
+		
+		//Numeric data types
+		int a=100;
+		int b=200;
+		System.out.println("the value of a is:"+a);
+		System.out.println("the value of b is:"+b);
+		System.out.println("the sum of a and b is:"+(a+b));
+		
+		byte by=125;
+		System.out.println(by);
+		
+		short sh=3535;
+		System.out.println(sh);
+		
+		long l=234567789;
+		System.out.println(l);
+		
+		float item_price=20.5F;
+		System.out.println(item_price);
+		
+		double dbl=123.76554;
+		System.out.println(dbl);
+		
+		char ch='A';
+		System.out.println(ch);
+		
+		String str="java";
+		System.out.println(str);
+		
+		boolean bl=true;
+		System.out.println(bl);
 
 	}
 
