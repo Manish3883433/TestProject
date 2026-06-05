04386ce94bc9e4684e7bdd88717d25f63d300cb0
Manish Manish <manish154154@gmail.com>
2026-06-05 17:33:04 +0530

    Session-1 SDET QA video

--- HelloWorld/src/test/HelloWorld.java
+++ HelloWorld/src/test/HelloWorld.java
@@ -4,6 +4,14 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to java");
+		/*System.out.println("Welcome to java"); syso ctrl+space 
+		System.out.println("Welcome to java");
+		System.out.println("Welcome to java");
+		System.out.println("Welcome to java");
+		System.out.println("Welcome to java");
+		System.out.println("Welcome to java");*/
 
 	}
 
