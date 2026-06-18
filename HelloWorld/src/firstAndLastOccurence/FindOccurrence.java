11a75e331ebfc68f32fa74f460dbe6df438bdc41
Manish Manish <manish154154@gmail.com>
2026-06-18 17:35:30 +0530

    Adds code in test branch

---
+++ HelloWorld/src/firstAndLastOccurence/FindOccurrence.java
@@ -0,0 +1,16 @@
+package firstAndLastOccurence;
+
+public class FindOccurrence {
+    public static void main(String[] args) {
+        String text = "amazonaanazzz";
+        
+        // indexOf() finds the first occurrence
+        int firstIndex = text.indexOf('a');
+        
+        // lastIndexOf() finds the last occurrence
+        int lastIndex = text.lastIndexOf('a');
+        
+        System.out.println("First occurrence of 'a' is at index: " + firstIndex);
+        System.out.println("Last occurrence of 'a' is at index: " + lastIndex);
+    }
+}
